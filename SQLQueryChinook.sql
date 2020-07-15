dbc69e047dc5ea7daa8ead8c48030309c91a2ea6
Brennen Tatum <bmar566@gmail.com>
2020-07-15 11:02:40 -0500

    completed question 3 chinook exercise

---
+++ SQLQueryChinook.sql
@@ -0,0 +1,12 @@
+﻿Select FirstName, LastName, CustomerId, Country from Customer where Country != 'USA'
+
+Select FirstName, LastName, CustomerId, Country from Customer where Country = 'Brazil'
+
+Select c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.BillingCountry 
+from Invoice i
+JOIN Customer c On i.CustomerId = c.CustomerId
+where BillingCountry = 'Brazil'
+order by c.FirstName
+
+
+
